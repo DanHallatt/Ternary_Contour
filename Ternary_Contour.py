d2f1dfde19299a71ca743e708fd3d9c8abbaf967
Dan Hallatt <38442319+DanHallatt@users.noreply.github.com>
2022-07-25 21:54:17 +0100

    Adding normalization option
    User-instigated normalization of contour values to [0,1] domain.

--- Ternary_Contour.py
+++ Ternary_Contour.py
@@ -14,7 +14,7 @@ from scipy.interpolate import griddata
 
 from Tern_Base import Tern_Base
 
-def Ternary_Contour(dataset, type, Colour, NumLevels, ContourValues, ContLines, ContColourFill, DataPointDisp, FigureSavePath, FileName):
+def Ternary_Contour(dataset, type, Colour, NumLevels, ContourValues, ContLines, ContColourFill, DataPointDisp, NormOption, FigureSavePath, FileName):
     """ Uses matplotlib to plot a ternary diagram
     ** dataset : in the form of a list of list [ [ [A11, B11, C11], ... [A1n, B1n, .. C1n] ], [ [A21, B21, C21], ... [A2n, B2n, .. C2n] ] , ... [ [Am1, Bm1, Cm1], ... [Amn, Bmn, .. Cmn] ] ], where n is the numer of datapoints in a given dataset and m is the number of datasets to plot individually. Even if m = 1, data must be in form of [ [A11, B11, .. C11], ... [A1n, B1n, C1n] ], where dataset[0] = [ [A11, B11, .. C11], ... [A1n, B1n, C1n] ] and not = [A11, B11, C11]. Note that for the ternary diagram, A is the top corner, B is the left corner, and C the right corner of the triangle (A = Si + Al, B = Fe, C = Mg for example)
         
@@ -27,6 +27,7 @@ def Ternary_Contour(dataset, type, Colour, NumLevels, ContourValues, ContLines, 
     ** ContLines : Either 'y' (yes) or 'n' (no), plot or not to plot the lines seperating contour levels.
     ** ContColourFill : Either 'y' (yes) or 'n' (no), to fill contour values with the colours defined in 'Colour' list.
     ** DataPointDisp : Either 'y' (yes) or 'n' (no), to plot the individual datapoints in the ternary diagram.
+    ** NormOption : Either 'y' (yes) or 'n' (no), to normalize the KDE values to be within [0,1] domain. Allows for interpretation of contour lines to be limit beyond which samples are less likely to be found beyond than the most-likely area.
     ** FigureSavePath : Path to folder location where figures should be saved. Must be in single quotations, example : '/Volumes/Samsung_T5/Experiment categories/Laser/Figures/'
     ** FileName : General name of files to be saved. Must be in single quotations, example : 'TEST_DataSet01'
     ** ContourValues : Either 'y' (yes) or 'n' (no), plot or not to plot the values defining each contour level (value is density value). Requires ContLines='y'.
@@ -36,42 +37,52 @@ def Ternary_Contour(dataset, type, Colour, NumLevels, ContourValues, ContLines, 
     ContourLineThickness = 0.4
     ContourLineStyle = '-'
     DataPointSize = 0.5
+    DataPointColour = 'dim-grey'
+    DataPointTransparency = 0.5
     
     # MAKING TERNARY DIAGRAM OF CONTOURS:
     fig, ax = Tern_Base(type)
-    for k in range(0,len(dataset)):
+    for k in range(0,len(dataset)): # Each loop is an individual dataset (corresponding to a single colour in 'Colour')
         tri_y=[]
         tri_x=[]
         Data_Indiv = dataset[k]
-        # Converting the 3D ternary data of [A, B, C] into 2D coordinates of [x,y] which correspond to the area within the ternary bounds. New data is thus tri_y and tri_x.
+        # Converting the 3D ternary data of [A, B, C] into 2D coordinates of [x,y]. New data is thus tri_y and tri_x.
         for i in range (len(Data_Indiv)): # Number of ternary coordinates (Si, Fe, Mg trio's).
             tri_y.append(Data_Indiv[i][0]/(Data_Indiv[i][0]+Data_Indiv[i][1]+Data_Indiv[i][2]))
             tri_x.append(np.array(-0.5)*(np.array(1.)-tri_y[i])+(Data_Indiv[i][2]/(Data_Indiv[i][0]+Data_Indiv[i][1]+Data_Indiv[i][2])))
-        y_Ternary = tri_y
-        x_Ternary = tri_x
-        xmin_Ternary, xmax_Ternary = -0.6, 0.6
-        ymin_Ternary, ymax_Ternary = -0.1, 1.1
+        y = tri_y
+        x = tri_x
+        xmin, xmax = -0.6, 0.6
+        ymin, ymax = -0.1, 1.1
 
         # Peform the kernel density estimate
-        xx_Ternary, yy_Ternary = np.mgrid[xmin_Ternary:xmax_Ternary:1000j, ymin_Ternary:ymax_Ternary:1000j]
-        positions_Ternary = np.vstack([xx_Ternary.ravel(), yy_Ternary.ravel()])
-        values_Ternary = np.vstack([x_Ternary, y_Ternary])
-        kernel_Ternary = st.gaussian_kde(values_Ternary)
-        f_Ternary = np.reshape(kernel_Ternary(positions_Ternary).T, xx_Ternary.shape)
+        kernel = st.gaussian_kde(np.vstack([x, y]))
+        xx, yy = np.mgrid[xmin:xmax:1000j, ymin:ymax:1000j]
+        f = kernel(np.vstack([xx.flatten(), yy.flatten()]))
+        if NormOption == 'y':
+            f = (f - f.min()) / (f.max() - f.min())
+        f = f.reshape(xx.shape)
+        
+        # Including all th possible user-specified combinations of displaying contour colour, lines, & contour values.
         if ContColourFill == 'y':
-            ContColour_Ternary = plt.contourf(xx_Ternary, yy_Ternary, f_Ternary, NumLevels, cmap=Colour[k], locator = ticker.MaxNLocator(prune = 'lower'), zorder=5)
+            ContColour = plt.contourf(xx, yy, f, NumLevels, cmap=Colour[k], locator = ticker.MaxNLocator(prune = 'lower', nbins=NumLevels), zorder=5)
         if ContLines == 'n' and ContourValues == 'y':
             ContourLineThickness = 0
-            cset_Ternary = plt.contour(xx_Ternary, yy_Ternary, f_Ternary, NumLevels, colors='k', alpha=1, linewidths = ContourLineThickness, linestyles = ContourLineStyle, locator = ticker.MaxNLocator(prune = 'lower'), zorder=10) # Drawing contour lines.
-            if ContourValues == 'y':
-                ax.clabel(cset_Ternary, inline=1, fontsize=ContourLabelTextSize, zorder=15) # Labelling contour levels within the contour lines.
+            if ContColourFill == 'y': # Gauruntee same levels as colour levels
+                cset = plt.contour(xx, yy, f, ContColour.levels, colors='k', alpha=1, linewidths = ContourLineThickness, linestyles = ContourLineStyle, zorder=10, extend='both') # Drawing contour lines.
+            else: #Making their own levels, without coloured levels.
+                cset = plt.contour(xx, yy, f, NumLevels, colors='k', alpha=1, linewidths = ContourLineThickness, linestyles = ContourLineStyle, locator = ticker.MaxNLocator(prune = 'lower', nbins=NumLevels), zorder=10) # Drawing contour lines.
+            ax.clabel(cset, inline=1, fontsize=ContourLabelTextSize, zorder=15) # Labelling contour levels within the contour lines.
         if ContLines == 'y':
-            cset_Ternary = plt.contour(xx_Ternary, yy_Ternary, f_Ternary, NumLevels, colors='k', alpha=1, linewidths = ContourLineThickness, linestyles = ContourLineStyle, locator = ticker.MaxNLocator(prune = 'lower'), zorder=10) # Drawing contour lines.
+            if ContColourFill == 'y': # Gauruntee same levels as colour levels.
+                cset = plt.contour(xx, yy, f, ContColour.levels, colors='k', alpha=1, linewidths = ContourLineThickness, linestyles = ContourLineStyle, zorder=10) # Drawing contour lines.
+            else:
+                cset = plt.contour(xx, yy, f, NumLevels, colors='k', alpha=1, linewidths = ContourLineThickness, linestyles = ContourLineStyle, locator = ticker.MaxNLocator(prune = 'lower', nbins=NumLevels), zorder=10) # Drawing contour lines.
             if ContourValues == 'y':
-                ax.clabel(cset_Ternary, inline=1, fontsize=ContourLabelTextSize, zorder=15) # Labelling contour levels within the contour lines.
+                ax.clabel(cset, inline=1, fontsize=ContourLabelTextSize, zorder=15) # Labelling contour levels within the contour lines.
         if DataPointDisp=='y':
-            ax.scatter(tri_x, tri_y, color='black', alpha=1, s=DataPointSize, zorder=5)
-            
+            ax.scatter(tri_x, tri_y, color=DataPointColour, alpha=DataPointTransparency, s=DataPointSize, zorder=5)
+
     ax.axis('off')
     fig.savefig(FigureSavePath + FileName +  '_Ternary_Contour_' + type + 'Axis.pdf')
 
